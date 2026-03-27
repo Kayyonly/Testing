44c73f7596df0ca97c8ea0e4cde0559370339c18
Kayyonly <kenzonly12@gmail.com>
2026-03-27 22:21:19 +0700

    Delete app/test-ytmusic-playlist.ts

--- app/test-ytmusic-playlist.ts
+++
@@ -1,8 +0,0 @@
-import { getYTMusicClient } from "@/lib/ytmusic";
-import { getYTMusicClient } from "../lib/ytmusic";
-async function testPlaylist() {
-  const ytmusic = await getYTMusicClient();
-  const playlist = await ytmusic.getPlaylist("PL4fGSI1pDJn5rWitrRWFKdm-ulaFiIyoK");
-  console.log("[test-ytmusic-playlist] Playlist data:", playlist);
-}
-testPlaylist().catch((error) => console.error("[test-ytmusic-playlist] Error:", error));
