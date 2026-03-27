cf9a33c0d8ba12216969034ed0ae24982c979981
Kayyonly <kenzonly12@gmail.com>
2026-03-27 22:23:42 +0700

    Delete app/test-ytmusic-artist.ts

--- app/test-ytmusic-artist.ts
+++
@@ -1,8 +0,0 @@
-import { getYTMusicClient } from "@/lib/ytmusic";
-import { getYTMusicClient } from "../lib/ytmusic";
-async function testArtistDetail() {
-  const ytmusic = await getYTMusicClient();
-  const artist = await ytmusic.getArtist("UCqECaJ8Gagnn7YCbPEzWH6g");
-  console.log("[test-ytmusic-artist] Artist detail:", artist);
-}
-testArtistDetail().catch((error) => console.error("[test-ytmusic-artist] Error:", error));
